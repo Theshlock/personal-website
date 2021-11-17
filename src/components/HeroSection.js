0013bba3a6aaaf3ee2439592472f0a07a10b728b
Theshlock <lockton.sam@gmail.com>
2021-11-18 08:29:43 +1100

    changed welcome text

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,8 +7,8 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
+      <h1>Samuel Lockton</h1>
+      <p>Welcome to my Portfolio</p>
       <div className='hero-btns'>
         <Button
           className='btns'
